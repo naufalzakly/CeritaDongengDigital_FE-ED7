086eed8e59d4d673d59ea7c45a596e96016eab0c
sellayuniar <synrr212@gmail.com>
2022-06-12 20:20:25 +0800

    react route dan lib dongeng

--- ceritadongeng/src/component/home/navbar/index.js
+++ ceritadongeng/src/component/home/navbar/index.js
@@ -1,87 +1,99 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import {Nav,ButtonGroup,Button,Container,Row,Col} from 'react-bootstrap'
-import './style.css'
-// import { FaBars, FaTimes } from 'react-icons/fa'
-import useState from 'react-hook-use-state';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+// import PageHome from "../../../pages/homepages";
+// import LibDongeng from "../../../pages/libDongeng/index";
+import { Nav, ButtonGroup, Button, Container, Row, Col } from "react-bootstrap";
+import "./style.css";
+// import { FaBars, FaTimes } from 'react-icons/fa';
+import useState from "react-hook-use-state";
+import { Link } from "react-router-dom";
 
-
-const NavbarHome = ({logo,garis,profil})=>{
-    
-
-    const [color, setColor] = useState(false)
-    const changeColor = () => {
-        if (window.scrollY >= 20){
-            setColor(true)
-        }else{
-            setColor(false)
-        }
+const NavbarHome = ({ logo, garis, profil }) => {
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    if (window.scrollY >= 20) {
+      setColor(true);
+    } else {
+      setColor(false);
     }
-    window.addEventListener('scroll', changeColor)
+  };
 
-    return(
-        <div className={color?'header header-bg': 'header'}>
+  window.addEventListener("scroll", changeColor);
 
-            <Container>
-                <Row>
-                    <Col>
-                        <Nav className="justify-content-start"  >
-                            <Nav.Item >
-                                    <Nav.Link  href="/home">
-                                        <img className='img_nav'
-                                            src = {logo}
-                                            
-                                            width={20}
-                                            height = {20} 
-                                            
-                                            alt = "pic"
-                                                    
-                                        />
-                                    </Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-         
-                        <br></br>
-                    </Col>
-                    <Col >
-                        <Nav className="justify-content-end"> 
-                            <Nav.Item>
-                                <Nav.Link href="#features" >
-                                    <Button   variant ="outline-dark" className="button">
-                                        <h2 className='text_nav'>Bergabung</h2>
-                                        
-                                    </Button>
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#memes">
-                                    <ButtonGroup   >
-                                        <Button className="button" variant ="outline-dark">
-                                            <img className='img_nav'
-                                                src = {garis}
-                                                height = {15}
-                                                width = {15}
-                                                alt = "pict"
-                                            />
-                                        </Button>
-                                        <Button  className="button" variant ="outline-dark">
-                                            <img  className='img_nav'
-                                                src = {profil}
-                                                width = {15}
-                                                height = {15}
-                                                alt = "pic"
-                                            />
-                                        </Button>
-                                    </ButtonGroup>
-                                </Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                            
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-        
-    )
-}
+  return (
+    // <Router>
+      <div className={color ? "header header-bg" : "header"}>
+        <Container>
+          <Row>
+            <Col>
+              <Nav className="justify-content-start">
+                <Nav.Item>
+                <Nav.Link href="#features">
+                    <img
+                      className="img_nav"
+                      src={logo}
+                      width={20}
+                      height={20}
+                      alt="pic"
+                    />
+                </Nav.Link>
+                </Nav.Item>
+              </Nav>
+
+              <br />
+            </Col>
+            <Col>
+              <Nav className="justify-content-end">
+
+                <Nav.Item>
+                  <Link to="/libDongeng">
+                    <Button variant="outline-dark" className="button">
+                      <h2 className="text_nav">Library Dongeng</h2>
+                    </Button>
+                  </Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link href="#features">
+                    <Button variant="outline-dark" className="button">
+                      <h2 className="text_nav">Bergabung</h2>
+                    </Button>
+                  </Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link href="#memes">
+                    <ButtonGroup>
+                      <Button className="button" variant="outline-dark">
+                        <img
+                          className="img_nav"
+                          src={garis}
+                          height={15}
+                          width={15}
+                          alt="pict"
+                        />
+                      </Button>
+                      <Button className="button" variant="outline-dark">
+                        <img
+                          className="img_nav"
+                          src={profil}
+                          width={15}
+                          height={15}
+                          alt="pic"
+                        />
+                      </Button>
+                    </ButtonGroup>
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+          </Row>
+        </Container>
+
+       
+      </div>
+ 
+
+  );
+};
 export default NavbarHome;
--- ceritadongeng/src/pages/homepages/index.js
+++ ceritadongeng/src/pages/homepages/index.js
@@ -1,27 +1,19 @@
 import Home from "../../component/home/slider";
 import React from 'react';
-import NavbarHome from '../../component/home/navbar';
-import Card_cerita from "../../component/home/card_cerita";
-import {Table} from 'react-bootstrap'
-
-import logo from '../../icon/garis.png';
-import garis from '../../icon/garis.png';
-import profile from '../../icon/profile.png';
+// import NavbarHome from '../../component/home/navbar';
+import CardCerita from "../../component/home/card_cerita";
+// import {Table} from 'react-bootstrap';
 import slider1 from '../../img/bebek_buruk_rupa_thumbnail.png';
 import slider2 from '../../img/kancil_buaya_thumbnail.png';
 import slider3 from '../../img/Kancil_dan_harimau_thumbnail.png';
-import slider4 from '../../img/kelinci_kura_thumbnail.png'
+import slider4 from '../../img/kelinci_kura_thumbnail.png';
 
 
 const PageHome = ()=>{
     return(
         <div className="Container">
             <div className="Home">
-                <NavbarHome
-                    logo = {logo}
-                    garis = {garis}
-                    profil = {profile}
-                />
+                
                 <Home
                     
                     slider1 = {slider1}
@@ -31,21 +23,21 @@ const PageHome = ()=>{
                     
                 />
                 
-                <Card_cerita
+                <CardCerita
                     title = {"Baru Rilis"}
                     cerita1 = {slider1}
                     cerita2 = {slider2}
                     cerita3 = {slider3}
                 />
 
-                <Card_cerita
+                <CardCerita
                     title = {"Cerita Terpopuler"}
                     cerita1 = {slider2}
                     cerita2 = {slider3}
                     cerita3 = {slider1}
                 />
 
-                <Card_cerita
+                <CardCerita
                     title = {"Segera Rilis"}
                     cerita1 = {slider2}
                     cerita2 = {slider1}
