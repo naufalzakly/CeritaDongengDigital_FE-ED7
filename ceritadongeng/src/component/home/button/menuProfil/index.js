e6e16326a6aab67a043012072c2e45ca46a4abd2
sellayuniar <synrr212@gmail.com>
2022-06-21 15:48:15 +0800

    dropdown menu belum tampil

--- ceritadongeng/src/component/home/button/menuProfil/index.js
+++ ceritadongeng/src/component/home/button/menuProfil/index.js
@@ -1,23 +1,25 @@
-import {
-    Nav,
-    Button,
-  } from "react-bootstrap";
-import { BsList, BsFillPersonFill} from "react-icons/bs";
+import { Nav, Dropdown } from "react-bootstrap";
+import { BsList, BsFillPersonFill } from "react-icons/bs";
+import "./index.css";
 
 const ButtonMenuProfil = () => {
-  
-    return(
-        <div>
-            <Nav.Item>
-                <Nav.Link href="#features">
-                  <Button variant="outline-dark" className="button">
-                    <BsList size="1.5em" className="mx-2" />
-                    <BsFillPersonFill size="1.5em" />
-                  </Button>
-                </Nav.Link>
-              </Nav.Item>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Nav.Item>
+        <Dropdown>
+          <Dropdown.Toggle variant="light" id="dropdown-basic" className=" mt-3">
+            <BsList /> <BsFillPersonFill/>
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu className="dropdown-menu">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Nav.Item>
+    </div>
+  );
+};
 
 export default ButtonMenuProfil;
