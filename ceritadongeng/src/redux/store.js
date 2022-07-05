1c908890504c25881ee03be66cd9c49089bd8536
Naufal Zakly Santoso <72109817+naufalzakly@users.noreply.github.com>
2022-07-05 12:30:42 +0700

    delete ceritadongeng/src/redux

--- ceritadongeng/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import ulasanReducer from './hooks/ulasanSlice';
-
-export const store = configureStore({
-  reducer: {
-    ulasan: ulasanReducer
-  }
-});
