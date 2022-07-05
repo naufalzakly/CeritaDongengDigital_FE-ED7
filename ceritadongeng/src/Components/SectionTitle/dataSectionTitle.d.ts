425eb8a347cc69944b6bd836208ff8abcaf903fe
Naufal Zakly Santoso <72109817+naufalzakly@users.noreply.github.com>
2022-07-05 13:44:09 +0700

    delete ceritadongeng/src

--- ceritadongeng/src/Components/SectionTitle/dataSectionTitle.d.ts
+++
@@ -1,3 +0,0 @@
-export type sectionTitleTypes = {
-    title: string,
-}
--- ceritadongeng/src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import ulasanReducer from './Hooks/ulasanSlice';
-
-export const store = configureStore({
-  reducer: {
-    ulasan: ulasanReducer
-  }
-});
